201657736bb60bd9dfedc755f01d2cf090474c2d
Amida88 <155386233+amida8@users.noreply.github.com>
2026-01-27 18:26:12 +0900

    supabaseClient.js を作成

---
+++ supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+  "https://wohmodtgphwwnwlxqmgt.supabase.co",
+  "sb_publishable_rYaVM0WjlXFfQj5wKJCzbg_2ZAML0RX"
+);
